d1780347f7cfeb47ff185e24474d87f1f1d91fe7
Ben Leggiero <BenLeggiero@Gmail.com>
2016-09-20 22:01:36 -0400

    Token app get!

--- Android-Studio-Project/app/src/main/java/org/bh/app/rentsplit/MainActivity.java
+++ Android-Studio-Project/app/src/main/java/org/bh/app/rentsplit/MainActivity.java
@@ -5,11 +5,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        WebView mainWebView = (WebView) findViewById(R.id.MainWebView);
+        mainWebView.setWebViewClient(new WebViewClient());
+        mainWebView.loadUrl("https://Soft.BHStudios.org/Rent-Split");
     }
 
     @Override
